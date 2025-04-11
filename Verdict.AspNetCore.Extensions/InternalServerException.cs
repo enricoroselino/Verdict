10a2362f25d4ec0b7697212c5f0294b5f1aa510c
enricoroselino <enricoroselino@gmail.com>
2025-04-11 21:57:09 +0700

    ✅ feat(Verdict.AspNetCore.Extensions): Add InternalServerException class

---
+++ Verdict.AspNetCore.Extensions/InternalServerException.cs
@@ -0,0 +1,10 @@
+﻿namespace Verdict.AspNetCore.Extensions;
+
+public class InternalServerException : Exception
+{
+    private const string ErrorMessage = "An unexpected error occurred while processing your request.";
+
+    public InternalServerException(string? message = null) : base(message ?? ErrorMessage)
+    {
+    }
+}
