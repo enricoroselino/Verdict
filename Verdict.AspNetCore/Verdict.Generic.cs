4e71bcda40f57a5cc367ce574499da1840560a7d
enricoroselino <enricoroselino@gmail.com>
2025-04-11 21:57:53 +0700

    ✅ feat(Verdict): Add Verdict and Verdict<T> classes

---
+++ Verdict.AspNetCore/Verdict.Generic.cs
@@ -0,0 +1,66 @@
+﻿namespace Verdict.AspNetCore;
+
+public class Verdict<T> : VerdictBase<T>
+{
+    protected Verdict()
+    {
+    }
+
+    internal Verdict(T payload) => Payload = payload;
+    protected Verdict(VerdictStatus status) => Status = status;
+    public static implicit operator T(Verdict<T> verdict) => verdict.Payload;
+    public static implicit operator Verdict<T>(T value) => new(value);
+
+    public static implicit operator Verdict<T>(Verdict verdict) => new(default(T)!)
+    {
+        Status = verdict.Status,
+        ValidationErrors = verdict.ValidationErrors,
+        ErrorMessage = verdict.ErrorMessage,
+        ErrorCode = verdict.ErrorCode,
+    };
+
+    public static explicit operator Verdict(Verdict<T> verdict) => new()
+    {
+        Status = verdict.Status,
+        ValidationErrors = verdict.ValidationErrors,
+        ErrorMessage = verdict.ErrorMessage,
+        ErrorCode = verdict.ErrorCode,
+    };
+
+    public static Verdict<T> Success(T value) => new(value);
+    public static Verdict<T> Created(T value) => new(VerdictStatus.Created) { Payload = value };
+
+    public static Verdict<T> Created(T value, string location) =>
+        new(VerdictStatus.Created) { Payload = value, Location = location };
+
+    public static Verdict<T> NoContent() => new(VerdictStatus.NoContent);
+
+    public static Verdict<T> NotFound(string errorMessage) =>
+        new(VerdictStatus.NotFound) { ErrorMessage = errorMessage };
+
+    public static Verdict<T> Forbidden(string? errorMessage = null) => new(VerdictStatus.Forbidden)
+        { ErrorMessage = errorMessage ?? "Forbidden" };
+
+    public static Verdict<T> Unauthorized(string? errorMessage = null, string? errorCode = null) =>
+        new(VerdictStatus.Unauthorized)
+        {
+            ErrorMessage = errorMessage ?? "Unauthorized",
+            ErrorCode = errorCode
+        };
+
+    public static Verdict<T> BadRequest(string errorMessage) =>
+        new(VerdictStatus.BadRequest) { ErrorMessage = errorMessage };
+
+    public static Verdict<T> BadRequest(Dictionary<string, string> errors) =>
+        new(VerdictStatus.BadRequest)
+            { ValidationErrors = errors, ErrorMessage = "One or more validation failures have occurred." };
+
+    public static Verdict<T> UnprocessableEntity(string errorMessage) =>
+        new(VerdictStatus.UnprocessableEntity) { ErrorMessage = errorMessage };
+
+    public static Verdict<T> Conflict(string errorMessage) =>
+        new(VerdictStatus.Conflict) { ErrorMessage = errorMessage };
+
+    public static Verdict<T> InternalError(string errorMessage) =>
+        new(VerdictStatus.InternalError) { ErrorMessage = errorMessage };
+}
---
+++ Verdict.AspNetCore/Verdict.cs
@@ -0,0 +1,48 @@
+﻿namespace Verdict.AspNetCore;
+
+public class Verdict : Verdict<Verdict>
+{
+    internal Verdict()
+    {
+    }
+
+
+    private Verdict(VerdictStatus status) : base(status)
+    {
+    }
+
+
+    public static Verdict Success() => new();
+    public static Verdict<T> Success<T>(T value) => new(value);
+    public static Verdict<T> Created<T>(T value) => new(value);
+    public new static Verdict NoContent() => new(VerdictStatus.NoContent);
+
+    public new static Verdict NotFound(string errorMessage) =>
+        new(VerdictStatus.NotFound) { ErrorMessage = errorMessage };
+
+    public new static Verdict Forbidden(string? errorMessage = null) => new(VerdictStatus.Forbidden)
+        { ErrorMessage = errorMessage ?? "Forbidden" };
+
+    public new static Verdict Unauthorized(string? errorMessage = null, string? errorCode = null) =>
+        new(VerdictStatus.Unauthorized)
+        {
+            ErrorMessage = errorMessage ?? "Unauthorized",
+            ErrorCode = errorCode,
+        };
+
+    public new static Verdict BadRequest(string errorMessage) =>
+        new(VerdictStatus.BadRequest) { ErrorMessage = errorMessage };
+
+    public new static Verdict BadRequest(Dictionary<string, string> errors) =>
+        new(VerdictStatus.BadRequest)
+            { ValidationErrors = errors, ErrorMessage = "One or more validation failures have occurred." };
+
+    public new static Verdict UnprocessableEntity(string errorMessage) =>
+        new(VerdictStatus.UnprocessableEntity) { ErrorMessage = errorMessage };
+
+    public new static Verdict Conflict(string errorMessage) =>
+        new(VerdictStatus.Conflict) { ErrorMessage = errorMessage };
+
+    public new static Verdict InternalError(string errorMessage) =>
+        new(VerdictStatus.InternalError) { ErrorMessage = errorMessage };
+}
