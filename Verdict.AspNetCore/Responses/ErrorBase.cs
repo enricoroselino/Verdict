01e61942619138bead78137d4b3fbabe64e08033
enricoroselino <enricoroselino@gmail.com>
2025-04-11 21:56:30 +0700

    ✅ feat(Responses): Add ErrorResponse and ErrorBase classes for consistent error handling.

---
+++ Verdict.AspNetCore/Responses/ErrorBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Verdict.AspNetCore.Responses;
+
+public class ErrorBase
+{
+    protected ErrorBase()
+    {
+    }
+
+    [JsonPropertyOrder(1)] public int StatusCode { get; protected set; }
+    [JsonPropertyOrder(2)] public string RequestId { get; protected set; } = string.Empty;
+    [JsonPropertyOrder(3)] public string Path { get; protected set; } = string.Empty;
+    [JsonPropertyOrder(4)] public string Message { get; protected set; } = string.Empty;
+    [JsonPropertyOrder(5)] public string? ErrorCode { get; protected set; }
+    [JsonPropertyOrder(6)] public Dictionary<string, string>? ValidationErrors { get; protected set; }
+}
---
+++ Verdict.AspNetCore/Responses/ErrorResponse.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Verdict.AspNetCore.Responses;
+
+[Serializable]
+public class ErrorResponse : ErrorBase
+{
+    private ErrorResponse() : base()
+    {
+    }
+
+    public const string ContentType = "application/json";
+
+    public static ErrorResponse Create(HttpContext context, int statusCode, string message)
+    {
+        context.Response.StatusCode = statusCode;
+        context.Response.ContentType = ContentType;
+
+        return new ErrorResponse()
+        {
+            StatusCode = statusCode,
+            Message = message,
+            RequestId = context.TraceIdentifier,
+            Path = context.Request.Path,
+        };
+    }
+
+    public void AddErrorCode(string errorCode) => ErrorCode = errorCode;
+    public void AddValidationErrors(Dictionary<string, string> errors) => ValidationErrors = errors;
+}
