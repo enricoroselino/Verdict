46eefe6532d573e5d3b83edd617e8ec3898bfcf8
Enrico Roselino <roselino.dev@gmail.com>
2025-05-28 15:31:47 +0700

    ✅ feat(Verdict): Add parameterless Failed() factory methods

--- Verdict/Failure.cs
+++ Verdict/Failure.cs
@@ -2,6 +2,10 @@ namespace Verdict;
 
 public class Failure : Reason
 {
+    public Failure() : base()
+    {
+    }
+
     public Failure(string message) : base(message)
     {
     }
--- Verdict/Verdict.cs
+++ Verdict/Verdict.cs
@@ -34,6 +34,7 @@ public class Verdict<T> : IVerdict
     internal Verdict(T value) => Payload = value;
 
     public static Verdict<T> Success(T value) => new Verdict<T>(value) { Reason = new Success() };
+    public static Verdict<T> Failed() => new Verdict<T>() { Reason = new Failure() };
     public static Verdict<T> Failed(string message) => new Verdict<T>() { Reason = new Failure(message) };
 
     public Verdict<T> WithReason(Action<Reason> configure)
@@ -51,6 +52,7 @@ public class Verdict : Verdict<Verdict>
 
     public static Verdict Success() => new Verdict() { Reason = new Success() };
     public static Verdict<T> Success<T>(T value) => new Verdict<T>(value) { Reason = new Success() };
+    public new static Verdict Failed() => new Verdict() { Reason = new Failure() };
     public new static Verdict Failed(string message) => new Verdict() { Reason = new Failure(message) };
 
     public new Verdict WithReason(Action<Reason> configure)
