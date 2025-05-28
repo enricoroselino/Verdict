da4678fe2b6520c400c65a39f60c4b1a253b863e
Enrico Roselino <roselino.dev@gmail.com>
2025-05-28 15:52:25 +0700

    ✅ feat(Verdict.AspNetCore): Add default error messages.

---
+++ Verdict.AspNetCore/DefaultMessage.cs
@@ -0,0 +1,13 @@
+namespace Verdict.AspNetCore;
+
+public static class DefaultMessage
+{
+    public const string NotFound = "The requested resource could not be found.";
+    public const string BadRequest = "Your request could not be processed due to invalid or missing data.";
+    public const string Unauthorized = "You must be authenticated to access this resource.";
+    public const string Forbidden = "You do not have permission to access this resource.";
+    public const string UnprocessableEntity = "Your request was understood but contains data that cannot be processed.";
+
+    public const string InternalServerError =
+        "The server encountered an error while processing your request. Please contact support if the problem persists.";
+}
