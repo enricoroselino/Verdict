eb19ea908ea8d9bb1b99145dccf17ed2a8cf359d
Enrico Roselino <roselino.dev@gmail.com>
2025-05-28 15:55:31 +0700

    ✨ feat(Verdict.AspNetCore): Use StatusEnum instead of StatusCodes for HTTP status codes.

--- Verdict.AspNetCore/VerdictExtension.cs
+++ Verdict.AspNetCore/VerdictExtension.cs
@@ -6,29 +6,33 @@ public static class VerdictExtension
 {
     public static Verdict<T> Ok<T>(this Verdict<T> verdict)
     {
-        return verdict.WithReason(r => r.AddMetadata(WebMetadata.StatusCode, StatusCodes.Status200OK));
+        return verdict.WithReason(r => r.AddMetadata(WebMetadata.StatusCode, StatusEnum.Ok));
     }
 
     public static Verdict<T> Created<T>(this Verdict<T> verdict, string location = "")
     {
         return verdict.WithReason(r =>
-            r.AddMetadata(WebMetadata.StatusCode, StatusCodes.Status201Created)
+            r.AddMetadata(WebMetadata.StatusCode, StatusEnum.Created)
                 .AddMetadata(WebMetadata.Location, location));
     }
 
     public static Verdict NoContent(this Verdict verdict)
     {
-        return verdict.WithReason(r => r.AddMetadata(WebMetadata.StatusCode, StatusCodes.Status204NoContent));
+        return verdict.WithReason(r => r.AddMetadata(WebMetadata.StatusCode, StatusEnum.NoContent));
     }
 
     public static Verdict NotFound(this Verdict verdict)
     {
-        return verdict.WithReason(r => r.AddMetadata(WebMetadata.ErrorCode, StatusCodes.Status404NotFound));
+        return verdict.WithReason(r =>
+            r.AddMetadata(WebMetadata.StatusCode, StatusEnum.NotFound)
+                .SetMessage(DefaultMessage.NotFound));
     }
 
     public static Verdict BadRequest(this Verdict verdict)
     {
-        return verdict.WithReason(r => r.AddMetadata(WebMetadata.ErrorCode, StatusCodes.Status400BadRequest));
+        return verdict.WithReason(r =>
+            r.AddMetadata(WebMetadata.StatusCode, StatusEnum.BadRequest)
+                .SetMessage(DefaultMessage.BadRequest));
     }
 
     public static Verdict BadRequest(this Verdict verdict, Dictionary<string, string> errors)
@@ -39,27 +43,42 @@ public static class VerdictExtension
 
     public static Verdict Unauthorized(this Verdict verdict)
     {
-        return verdict.WithReason(r => r.AddMetadata(WebMetadata.ErrorCode, StatusCodes.Status401Unauthorized));
+        return verdict.WithReason(r =>
+            r.AddMetadata(WebMetadata.StatusCode, StatusEnum.Unauthorized)
+                .SetMessage(DefaultMessage.Unauthorized));
     }
 
     public static Verdict Unauthorized(this Verdict verdict, string errorCode)
     {
         return verdict.Unauthorized()
-            .WithReason(r => r.AddMetadata(WebMetadata.ErrorCode, errorCode));
+            .WithReason(r => r.AddMetadata(WebMetadata.StatusCode, errorCode));
     }
 
     public static Verdict Forbidden(this Verdict verdict)
     {
-        return verdict.WithReason(r => r.AddMetadata(WebMetadata.ErrorCode, StatusCodes.Status403Forbidden));
+        return verdict.WithReason(r =>
+            r.AddMetadata(WebMetadata.StatusCode, StatusEnum.Forbidden)
+                .SetMessage(DefaultMessage.Forbidden));
     }
 
     public static Verdict UnprocessableEntity(this Verdict verdict)
     {
-        return verdict.WithReason(r => r.AddMetadata(WebMetadata.ErrorCode, StatusCodes.Status422UnprocessableEntity));
+        return verdict.WithReason(r =>
+            r.AddMetadata(WebMetadata.StatusCode, StatusEnum.UnprocessableEntity)
+                .SetMessage(DefaultMessage.UnprocessableEntity));
+    }
+
+    public static Verdict Conflict(this Verdict verdict)
+    {
+        return verdict.WithReason(r =>
+            r.AddMetadata(WebMetadata.StatusCode, StatusEnum.Conflict)
+                .SetMessage(DefaultMessage.Conflict));
     }
 
     public static Verdict InternalServer(this Verdict verdict)
     {
-        return verdict.WithReason(r => r.AddMetadata(WebMetadata.ErrorCode, StatusCodes.Status500InternalServerError));
+        return verdict.WithReason(r =>
+            r.AddMetadata(WebMetadata.StatusCode, StatusEnum.InternalServerError)
+                .SetMessage(DefaultMessage.InternalServerError));
     }
 }
