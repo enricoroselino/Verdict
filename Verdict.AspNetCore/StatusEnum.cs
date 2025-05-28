929cb6861b57ae76a604d09dc7593c0abf9c7c16
Enrico Roselino <roselino.dev@gmail.com>
2025-05-28 16:01:29 +0700

    Refactor(StatusEnum): Remove Microsoft.AspNetCore.Http dependency from StatusEnum

--- Verdict.AspNetCore/StatusEnum.cs
+++ Verdict.AspNetCore/StatusEnum.cs
@@ -1,17 +1,15 @@
-using Microsoft.AspNetCore.Http;
-
 namespace Verdict.AspNetCore;
 
 public enum StatusEnum
 {
-    Ok = StatusCodes.Status200OK,
-    NoContent = StatusCodes.Status204NoContent,
-    Created = StatusCodes.Status201Created,
-    Conflict = StatusCodes.Status409Conflict,
-    Forbidden = StatusCodes.Status403Forbidden,
-    NotFound = StatusCodes.Status404NotFound,
-    UnprocessableEntity = StatusCodes.Status422UnprocessableEntity,
-    Unauthorized = StatusCodes.Status401Unauthorized,
-    BadRequest = StatusCodes.Status400BadRequest,
-    InternalServerError = StatusCodes.Status500InternalServerError,
+    Ok = 200,
+    NoContent = 204,
+    Created = 201,
+    Conflict = 409,
+    Forbidden = 403,
+    NotFound = 404,
+    UnprocessableEntity = 422,
+    Unauthorized = 401,
+    BadRequest = 400,
+    InternalServerError = 500,
 }
