677b4ae0e7dc9dbf526514fe561f696d865459bd
Enrico Roselino <roselino.dev@gmail.com>
2025-05-30 08:41:28 +0700

    🐛 fix(Verdict.Web/Models/Error.cs): Remove unnecessary properties from Error model.

--- Verdict.Web/Models/Error.cs
+++ Verdict.Web/Models/Error.cs
@@ -6,11 +6,9 @@ namespace Verdict.Web.Models;
 public class Error
 {
     [JsonPropertyOrder(1)] public int StatusCode { get; protected set; }
-    [JsonPropertyOrder(2)] public string RequestId { get; protected set; } = string.Empty;
-    [JsonPropertyOrder(3)] public string Path { get; protected set; } = string.Empty;
-    [JsonPropertyOrder(4)] public string Message { get; protected set; } = string.Empty;
-    [JsonPropertyOrder(5)] public string? ErrorCode { get; protected set; }
-    [JsonPropertyOrder(6)] public Dictionary<string, string>? ValidationErrors { get; protected set; }
+    [JsonPropertyOrder(2)] public string Message { get; protected set; } = string.Empty;
+    [JsonPropertyOrder(3)] public string? ErrorCode { get; protected set; }
+    [JsonPropertyOrder(4)] public Dictionary<string, string>? ValidationErrors { get; protected set; }
 
     private Error()
     {
