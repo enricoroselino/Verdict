d2bd51dd56932ee5d1a186b06e9eea2533476f92
Enrico Roselino <roselino.dev@gmail.com>
2025-05-30 08:10:12 +0700

    ✅ refactor(Pagination): Remove Ardalis.GuardClauses dependency

--- Verdict.Web/Models/Pagination.cs
+++ Verdict.Web/Models/Pagination.cs
@@ -1,6 +1,4 @@
-﻿using Ardalis.GuardClauses;
-
-namespace Verdict.Web.Models;
+﻿namespace Verdict.Web.Models;
 
 public record Pagination
 {
@@ -10,9 +8,10 @@ public record Pagination
 
     public Pagination(int page, int pageSize = 10)
     {
-        Page = Guard.Against.NegativeOrZero(page, nameof(page),
-            exceptionCreator: () => new ArgumentException("Page must be greater than zero."));
-        PageSize = Guard.Against.NegativeOrZero(pageSize, nameof(pageSize),
-            exceptionCreator: () => new ArgumentException("Page size must be greater than zero."));
+        if (page <= 0) throw new ArgumentException("Page must be greater than zero.");
+        if (pageSize <= 0) throw new ArgumentException("Page size must be greater than zero.");
+
+        Page = page;
+        PageSize = pageSize;
     }
 }
