8659708c94d6785a6ab2bb84ccae202edd24d617
Enrico Roselino <roselino.dev@gmail.com>
2025-05-30 09:21:57 +0700

    ✅ refactor(Response): Rename ResponseDto to Response and improve its usage

---
+++ Verdict.Web.AspNetCore/ResponseExtension.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Http;
+
+namespace Verdict.Web.AspNetCore;
+
+public static class ResponseExtension
+{
+    public static IResult ToResult<T>(this Response<T> response)
+    {
+        return Results.Json(response, statusCode: response.StatusCode);
+    }
+
+    public static IResult ToResult(this Response response)
+    {
+        return Results.Json(response, statusCode: response.StatusCode);
+    }
+}
---
+++ Verdict.Web/Response.cs
@@ -0,0 +1,33 @@
+﻿using Verdict.Web.Models;
+
+namespace Verdict.Web;
+
+public abstract class Response
+{
+    public int StatusCode { get; private init; }
+    public Error? Error { get; init; }
+
+    public static Response<TData> Build<TData>(int statusCode, TData data, Meta? meta = null) =>
+        meta is null
+            ? new Response<TData>(data) { StatusCode = statusCode }
+            : new Response<TData>(data, meta) { StatusCode = statusCode };
+
+    public static Response<TData> Build<TData>(int statusCode, Error error) =>
+        new Response<TData>(error) { StatusCode = statusCode };
+}
+
+[Serializable]
+public class Response<TData> : Response
+{
+    internal Response(TData? data, Meta meta)
+    {
+        Data = data;
+        Meta = meta;
+    }
+
+    internal Response(TData? data) => Data = data;
+    internal Response(Error error) => Error = error;
+
+    public TData? Data { get; init; }
+    public Meta? Meta { get; init; }
+}
