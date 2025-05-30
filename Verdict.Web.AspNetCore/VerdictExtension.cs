bad0b76ac6fbc8f2b2b5b54c16358ee919bee069
Enrico Roselino <roselino.dev@gmail.com>
2025-05-30 11:21:11 +0700

    ✅ refactor(Verdict.Web): Simplify Verdict response handling

--- Verdict.Web.AspNetCore/VerdictExtension.cs
+++ Verdict.Web.AspNetCore/VerdictExtension.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Http;
-using Verdict.Web.Constants;
 using Verdict.Web.Models;
-using StatusCodes = Verdict.Web.Constants.StatusCodes;
 
 namespace Verdict.Web.AspNetCore;
 
@@ -17,34 +15,28 @@ public static class VerdictExtension
 
     private static IResult ToSuccess(IVerdict verdict)
     {
-        var metadata = verdict.GetContext().Metadata;
+        var context = verdict.GetContext();
 
-        var statusCode = (int)(StatusCodes)metadata[WebMetadataConstant.StatusCode];
-        var meta = metadata.TryGetValue(WebMetadataConstant.Meta, out var metaObj) && metaObj is Meta castedMeta
-            ? castedMeta
-            : null;
+        var statusCode = context.GetStatusCode();
+        var meta = context.GetMeta();
+        var payload = verdict.GetValue();
 
-        var response = Response.Success(verdict.GetValue(), meta, statusCode);
+        var response = Response.Success(payload, meta, statusCode);
         return response.ToResult();
     }
 
     private static IResult ToError(IVerdict verdict)
     {
-        var metadata = verdict.GetContext().Metadata;
         var context = verdict.GetContext();
 
-        var statusCode = (int)(StatusCodes)metadata[WebMetadataConstant.StatusCode];
-        var errorCode = metadata.TryGetValue(WebMetadataConstant.ErrorCode, out var errorObj)
-            ? errorObj as string
-            : null;
-        var validationErrors = metadata.TryGetValue(WebMetadataConstant.ValidationErrors, out var validationObj)
-            ? validationObj as Dictionary<string, string>
-            : null;
+        var statusCode = context.GetStatusCode();
+        var errorCode = context.GetErrorCode();
+        var validationErrors = context.GetValidationErrors();
 
         var error = Error.Create(context.Message);
         if (errorCode is not null) error.AddErrorCode(errorCode);
         if (validationErrors != null && validationErrors.Count > 0) error.AddValidationErrors(validationErrors);
-        
+
         var response = Response.Failed(error, statusCode);
         return response.ToResult();
     }
---
+++ Verdict.Web/VerdictWebContextHelper.cs
@@ -0,0 +1,42 @@
+using Verdict.Web.Constants;
+using Verdict.Web.Models;
+
+namespace Verdict.Web;
+
+public static class VerdictWebContextHelper
+{
+    public static Meta? GetMeta(this IVerdictContext context)
+    {
+        var metadata = context.Metadata;
+        var meta = metadata.TryGetValue(WebMetadataConstant.Meta, out var metaObj) && metaObj is Meta castedMeta
+            ? castedMeta
+            : null;
+
+        return meta;
+    }
+
+    public static int GetStatusCode(this IVerdictContext context)
+    {
+        return (int)(StatusCodes)context.Metadata[WebMetadataConstant.StatusCode];
+    }
+
+    public static string? GetErrorCode(this IVerdictContext context)
+    {
+        var metadata = context.Metadata;
+        var errorCode = metadata.TryGetValue(WebMetadataConstant.ErrorCode, out var errorObj)
+            ? errorObj as string
+            : null;
+
+        return errorCode;
+    }
+
+    public static Dictionary<string, string>? GetValidationErrors(this IVerdictContext context)
+    {
+        var metadata = context.Metadata;
+        var errors = metadata.TryGetValue(WebMetadataConstant.ValidationErrors, out var validationObj)
+            ? validationObj as Dictionary<string, string>
+            : null;
+
+        return errors;
+    }
+}
