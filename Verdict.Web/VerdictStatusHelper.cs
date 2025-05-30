c5028236ed3bf1267ff5857afc0801934dab428f
Enrico Roselino <roselino.dev@gmail.com>
2025-05-30 10:55:36 +0700

    ✨ feat(VerdictStatusHelper): Add Meta to Ok method and use AddContext instead of AddMetadata.

--- Verdict.Web/VerdictStatusHelper.cs
+++ Verdict.Web/VerdictStatusHelper.cs
@@ -1,37 +1,40 @@
 using Verdict.Web.Constants;
+using Verdict.Web.Models;
 
 namespace Verdict.Web;
 
 public static class VerdictStatusHelper
 {
-    public static Verdict<T> Ok<T>(this Verdict<T> verdict)
+    public static Verdict<T> Ok<T>(this Verdict<T> verdict, Meta? meta = null)
     {
-        return verdict.WithContext(r => r.AddMetadata(ContextConstant.StatusCode, StatusCodes.Ok));
+        verdict.WithContext(r => r.AddContext(ContextConstant.StatusCode, StatusCodes.Ok));
+        if (meta is not null) verdict.WithContext(r => r.AddContext(ContextConstant.Meta, meta));
+        return verdict;
     }
 
     public static Verdict<T> Created<T>(this Verdict<T> verdict, string location = "")
     {
         return verdict.WithContext(r =>
-            r.AddMetadata(ContextConstant.StatusCode, StatusCodes.Created)
-                .AddMetadata(ContextConstant.Location, location));
+            r.AddContext(ContextConstant.StatusCode, StatusCodes.Created)
+                .AddContext(ContextConstant.Location, location));
     }
 
     public static Verdict NoContent(this Verdict verdict)
     {
-        return verdict.WithContext(r => r.AddMetadata(ContextConstant.StatusCode, StatusCodes.NoContent));
+        return verdict.WithContext(r => r.AddContext(ContextConstant.StatusCode, StatusCodes.NoContent));
     }
 
     public static Verdict NotFound(this Verdict verdict)
     {
         return verdict.WithContext(r =>
-            r.AddMetadata(ContextConstant.StatusCode, StatusCodes.NotFound)
+            r.AddContext(ContextConstant.StatusCode, StatusCodes.NotFound)
                 .SetMessage(DefaultMessage.NotFound));
     }
 
     public static Verdict BadRequest(this Verdict verdict)
     {
         return verdict.WithContext(r =>
-            r.AddMetadata(ContextConstant.StatusCode, StatusCodes.BadRequest)
+            r.AddContext(ContextConstant.StatusCode, StatusCodes.BadRequest)
                 .SetMessage(DefaultMessage.BadRequest));
     }
 
@@ -44,41 +47,41 @@ public static class VerdictStatusHelper
     public static Verdict Unauthorized(this Verdict verdict)
     {
         return verdict.WithContext(r =>
-            r.AddMetadata(ContextConstant.StatusCode, StatusCodes.Unauthorized)
+            r.AddContext(ContextConstant.StatusCode, StatusCodes.Unauthorized)
                 .SetMessage(DefaultMessage.Unauthorized));
     }
 
     public static Verdict Unauthorized(this Verdict verdict, string errorCode)
     {
         return verdict.Unauthorized()
-            .WithContext(r => r.AddMetadata(ContextConstant.ErrorCode, errorCode));
+            .WithContext(r => r.AddContext(ContextConstant.ErrorCode, errorCode));
     }
 
     public static Verdict Forbidden(this Verdict verdict)
     {
         return verdict.WithContext(r =>
-            r.AddMetadata(ContextConstant.StatusCode, StatusCodes.Forbidden)
+            r.AddContext(ContextConstant.StatusCode, StatusCodes.Forbidden)
                 .SetMessage(DefaultMessage.Forbidden));
     }
 
     public static Verdict UnprocessableEntity(this Verdict verdict)
     {
         return verdict.WithContext(r =>
-            r.AddMetadata(ContextConstant.StatusCode, StatusCodes.UnprocessableEntity)
+            r.AddContext(ContextConstant.StatusCode, StatusCodes.UnprocessableEntity)
                 .SetMessage(DefaultMessage.UnprocessableEntity));
     }
 
     public static Verdict Conflict(this Verdict verdict)
     {
         return verdict.WithContext(r =>
-            r.AddMetadata(ContextConstant.StatusCode, StatusCodes.Conflict)
+            r.AddContext(ContextConstant.StatusCode, StatusCodes.Conflict)
                 .SetMessage(DefaultMessage.Conflict));
     }
 
     public static Verdict InternalServer(this Verdict verdict)
     {
         return verdict.WithContext(r =>
-            r.AddMetadata(ContextConstant.StatusCode, StatusCodes.InternalServerError)
+            r.AddContext(ContextConstant.StatusCode, StatusCodes.InternalServerError)
                 .SetMessage(DefaultMessage.InternalServerError));
     }
 }
