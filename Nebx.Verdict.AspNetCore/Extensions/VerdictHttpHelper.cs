f8686761456d964ba7f0543a67047ac111352756
enricoroselino <enricoroselino@gmail.com>
2025-06-12 09:28:31 +0700

    feat(extensions): add `VerdictHttpHelper` for setting HTTP errors
    Adds a new static class `VerdictHttpHelper` to the `Extensions` folder.  This class provides methods for setting HTTP status codes, messages, and potential error details for `Verdict` and `Verdict<T>` objects.  This allows for more consistent and structured error handling.

---
+++ Nebx.Verdict.AspNetCore/Extensions/VerdictHttpHelper.cs
@@ -0,0 +1,44 @@
+﻿using Nebx.Verdict.AspNetCore.Constants;
+
+namespace Nebx.Verdict.AspNetCore.Extensions;
+
+public static class VerdictHttpHelper
+{
+    internal static Verdict<T> SetHttpError<T>(
+        this Verdict<T> verdict,
+        HttpStatusCodes statusCode,
+        string message,
+        IReadOnlyDictionary<string, string>? errors = null)
+    {
+        verdict.SetStatusCode(statusCode);
+        verdict.SetMessage(message);
+        if (errors != null) verdict.WithErrors(errors);
+
+        return verdict;
+    }
+
+    internal static Verdict SetHttpError(
+        this Verdict verdict,
+        HttpStatusCodes statusCode,
+        string message,
+        IReadOnlyDictionary<string, string>? errors = null)
+    {
+        verdict.SetStatusCode(statusCode);
+        verdict.SetMessage(message);
+        if (errors != null) verdict.WithErrors(errors);
+
+        return verdict;
+    }
+
+    internal static Verdict SetStatusCode(this Verdict verdict, HttpStatusCodes statusCode)
+    {
+        var metadata = new Dictionary<string, object> { [HttpKeys.StatusCode] = statusCode };
+        return verdict.WithMetadata(metadata);
+    }
+
+    internal static Verdict<T> SetStatusCode<T>(this Verdict<T> verdict, HttpStatusCodes statusCode)
+    {
+        var metadata = new Dictionary<string, object> { [HttpKeys.StatusCode] = statusCode };
+        return verdict.WithMetadata(metadata);
+    }
+}
