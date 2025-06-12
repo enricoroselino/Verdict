32a6d1c0ca51c541d29b310e4481c91e5b05fb53
enricoroselino <enricoroselino@gmail.com>
2025-06-12 09:20:16 +0700

    fix(ResponseDtoExtension): use correct constants
    Updates `ResponseDtoExtension` to use the correct constants for serializer options and content type.  Replaces `VerdictSerializerOption.MinimalApi` and `VerdictContentType.Json` with `SerializerOptions.MinimalApi` and `HttpContentTypes.Json`, respectively, ensuring consistency and reducing potential errors.

--- Nebx.Verdict.AspNetCore/Extensions/ResponseDtoExtension.cs
+++ Nebx.Verdict.AspNetCore/Extensions/ResponseDtoExtension.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Nebx.Verdict.AspNetCore.Constants;
 using Nebx.Verdict.AspNetCore.Models;
+using StatusCodes = Microsoft.AspNetCore.Http.StatusCodes;
 
 namespace Nebx.Verdict.AspNetCore.Extensions;
 
@@ -9,7 +10,7 @@ public static class ResponseDtoExtension
     public static IResult ToMinimalApiResult(this ResponseDto dto) =>
         Results.Json(
             dto,
-            options: VerdictSerializerOption.MinimalApi,
-            contentType: VerdictContentType.Json,
+            options: SerializerOptions.MinimalApi,
+            contentType: HttpContentTypes.Json,
             statusCode: StatusCodes.Status200OK);
 }
