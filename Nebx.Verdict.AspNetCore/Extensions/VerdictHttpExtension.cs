961b5ca06c0d0c8fc6a3f61c2d3ba4d5cd5496ee
enricoroselino <enricoroselino@gmail.com>
2025-06-12 09:19:25 +0700

    fix(VerdictHttpExtension): use HttpStatusCodes enum for status codes
    Replace `int` with `HttpStatusCodes` enum for better type safety and readability when setting HTTP status codes.  This change improves maintainability and reduces the risk of errors.

--- Nebx.Verdict.AspNetCore/Extensions/VerdictHttpExtension.cs
+++ Nebx.Verdict.AspNetCore/Extensions/VerdictHttpExtension.cs
@@ -6,113 +6,123 @@ public static class VerdictHttpExtension
 {
     private static Verdict<T> SetHttpError<T>(
         this Verdict<T> verdict,
-        int statusCode,
+        HttpStatusCodes statusCode,
         string message,
         IReadOnlyDictionary<string, string>? errors = null)
     {
-        var metadata = new Dictionary<string, object> { [HttpKeys.StatusCode] = statusCode };
+        verdict.SetStatusCode(statusCode);
+        verdict.SetMessage(message);
         if (errors != null) verdict.WithErrors(errors);
 
-        return verdict
-            .WithMetadata(metadata)
-            .SetMessage(message);
+        return verdict;
     }
 
     private static Verdict SetHttpError(
         this Verdict verdict,
-        int statusCode,
+        HttpStatusCodes statusCode,
         string message,
         IReadOnlyDictionary<string, string>? errors = null)
     {
-        var metadata = new Dictionary<string, object> { [HttpKeys.StatusCode] = statusCode };
+        verdict.SetStatusCode(statusCode);
+        verdict.SetMessage(message);
         if (errors != null) verdict.WithErrors(errors);
 
-        return verdict
-            .WithMetadata(metadata)
-            .SetMessage(message);
+        return verdict;
+    }
+
+    private static Verdict SetStatusCode(this Verdict verdict, HttpStatusCodes statusCode)
+    {
+        var metadata = new Dictionary<string, object> { [HttpKeys.StatusCode] = statusCode };
+        return verdict.WithMetadata(metadata);
+    }
+
+    private static Verdict<T> SetStatusCode<T>(this Verdict<T> verdict, HttpStatusCodes statusCode)
+    {
+        var metadata = new Dictionary<string, object> { [HttpKeys.StatusCode] = statusCode };
+        return verdict.WithMetadata(metadata);
     }
 
     public static Verdict<T> NotFound<T>(this Verdict<T> verdict)
     {
-        verdict.SetHttpError((int)HttpStatusCodes.NotFound, DefaulHttpMessages.NotFound);
+        verdict.SetHttpError(HttpStatusCodes.NotFound, DefaulHttpMessages.NotFound);
         return verdict;
     }
 
     public static Verdict NotFound(this Verdict verdict)
     {
-        verdict.SetHttpError((int)HttpStatusCodes.NotFound, DefaulHttpMessages.NotFound);
+        verdict.SetHttpError(HttpStatusCodes.NotFound, DefaulHttpMessages.NotFound);
         return verdict;
     }
 
     public static Verdict<T> BadRequest<T>(this Verdict<T> verdict, IReadOnlyDictionary<string, string>? errors = null)
     {
-        verdict.SetHttpError((int)HttpStatusCodes.BadRequest, DefaulHttpMessages.BadRequest, errors);
+        verdict.SetHttpError(HttpStatusCodes.BadRequest, DefaulHttpMessages.BadRequest, errors);
         return verdict;
     }
 
     public static Verdict BadRequest(this Verdict verdict, IReadOnlyDictionary<string, string>? errors = null)
     {
-        verdict.SetHttpError((int)HttpStatusCodes.BadRequest, DefaulHttpMessages.BadRequest, errors);
+        verdict.SetHttpError(HttpStatusCodes.BadRequest, DefaulHttpMessages.BadRequest, errors);
         return verdict;
     }
 
     public static Verdict<T> Forbidden<T>(this Verdict<T> verdict)
     {
-        verdict.SetHttpError((int)HttpStatusCodes.Forbidden, DefaulHttpMessages.Forbidden);
+        verdict.SetHttpError(HttpStatusCodes.Forbidden, DefaulHttpMessages.Forbidden);
         return verdict;
     }
 
     public static Verdict Forbidden(this Verdict verdict)
     {
-        verdict.SetHttpError((int)HttpStatusCodes.Forbidden, DefaulHttpMessages.Forbidden);
+        verdict.SetHttpError(HttpStatusCodes.Forbidden, DefaulHttpMessages.Forbidden);
         return verdict;
     }
 
     public static Verdict<T> Unauthorized<T>(this Verdict<T> verdict)
     {
-        verdict.SetHttpError((int)HttpStatusCodes.Unauthorized, DefaulHttpMessages.Unauthorized);
+        verdict.SetHttpError(HttpStatusCodes.Unauthorized, DefaulHttpMessages.Unauthorized);
         return verdict;
     }
 
     public static Verdict Unauthorized(this Verdict verdict)
     {
-        verdict.SetHttpError((int)HttpStatusCodes.Unauthorized, DefaulHttpMessages.Unauthorized);
+        verdict.SetHttpError(HttpStatusCodes.Unauthorized, DefaulHttpMessages.Unauthorized);
         return verdict;
     }
 
     public static Verdict<T> UnprocessableEntity<T>(this Verdict<T> verdict)
     {
-        verdict.SetHttpError((int)HttpStatusCodes.UnprocessableEntity, DefaulHttpMessages.UnprocessableEntity);
+        verdict.SetHttpError(HttpStatusCodes.UnprocessableEntity, DefaulHttpMessages.UnprocessableEntity);
         return verdict;
     }
 
     public static Verdict UnprocessableEntity(this Verdict verdict)
     {
-        verdict.SetHttpError((int)HttpStatusCodes.UnprocessableEntity, DefaulHttpMessages.UnprocessableEntity);
+        verdict.SetHttpError(HttpStatusCodes.UnprocessableEntity, DefaulHttpMessages.UnprocessableEntity);
         return verdict;
     }
 
     public static Verdict<T> Conflict<T>(this Verdict<T> verdict)
     {
-        verdict.SetHttpError((int)HttpStatusCodes.Conflict, DefaulHttpMessages.Conflict);
+        verdict.SetHttpError(HttpStatusCodes.Conflict, DefaulHttpMessages.Conflict);
         return verdict;
     }
 
     public static Verdict Conflict(this Verdict verdict)
     {
-        verdict.SetHttpError((int)HttpStatusCodes.Conflict, DefaulHttpMessages.Conflict);
+        verdict.SetHttpError(HttpStatusCodes.Conflict, DefaulHttpMessages.Conflict);
         return verdict;
     }
 
     public static Verdict<T> InternalError<T>(this Verdict<T> verdict)
     {
-        verdict.SetHttpError((int)HttpStatusCodes.InternalServerError, DefaulHttpMessages.InternalServerError);
+        verdict.SetHttpError(HttpStatusCodes.InternalServerError, DefaulHttpMessages.InternalServerError);
         return verdict;
     }
 
     public static Verdict InternalError(this Verdict verdict)
     {
-        verdict.SetHttpError((int)HttpStatusCodes.InternalServerError, DefaulHttpMessages.InternalServerError);
+        verdict.SetHttpError(HttpStatusCodes.InternalServerError, DefaulHttpMessages.InternalServerError);
         return verdict;
     }
 }
